e785ca6fb029c4a4baef14a4fd8228845942f8ab
Posoroko <eric@posoroko.com>
2023-01-17 23:01:10 +0100

    againnnn

--- pages/prestations/[type]/index.vue
+++ pages/prestations/[type]/index.vue
@@ -107,7 +107,7 @@ header {
     position: absolute;
     top: 0;
     left: 0;
-    background-image: url("/images/deco/motif feuille.jpg");
+    background-image: url("/images/deco/motif-feuille.jpg");
     opacity: 0.08;
     z-index: -1;
 }
