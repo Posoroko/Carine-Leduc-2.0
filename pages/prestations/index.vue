e1dbbfbae405a45b35096a4cb408554e7ebf0f7d
Posoroko <eric@posoroko.com>
2023-01-26 20:04:53 +0100

    small correction on file name

--- pages/prestations/index.vue
+++ pages/prestations/index.vue
@@ -60,7 +60,7 @@ const { data: prestations } = await useAsyncData(
     position: absolute;
     top: 0;
     left: 0;
-    background-image: url("/images/deco/motif feuille.jpg");
+    background-image: url("/images/deco/motif-feuille.jpg");
     opacity: 0.08;
     z-index: -1;
 }
